1c30643a129d4b97e724b5ba0382b7def2e58aab
Philippe Gerhardt <philippe@sutoiku.com>
2024-12-23 17:11:29 +0100

    Cleanup

--- src/librarymanagementsystem/utils/viewport.py
+++ src/librarymanagementsystem/utils/viewport.py
@@ -1,7 +1,3 @@
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QAbstractItemView
-
-
 def update_viewport(table_view, model):
     # Get the current sort column and order
     sort_column = table_view.horizontalHeader().sortIndicatorSection()
@@ -14,13 +10,3 @@ def update_viewport(table_view, model):
 
     # Reapply the sort order
     table_view.sortByColumn(sort_column, sort_order)
-
-
-def setup_table(table_view, model):
-    table_view.setModel(model)
-    table_view.resizeColumnsToContents()
-    table_view.setSortingEnabled(True)
-    table_view.sortByColumn(1, Qt.SortOrder.AscendingOrder)
-    table_view.setColumnHidden(0, True)
-    table_view.verticalHeader().setVisible(False)
-    table_view.setSelectionBehavior(QAbstractItemView.SelectionBehavior.SelectRows)
